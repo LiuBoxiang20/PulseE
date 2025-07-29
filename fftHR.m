edb5ab77bdeffcef1b51b7a743ab4710aef23277
cyh <675104929@qq.com>
2025-07-29 22:14:16 +0800

    Add fftHR.m

---
+++ fftHR.m
@@ -0,0 +1,22 @@
+function   [HR, freq, P1, f] = FFT_HR_Nfig(x, Fs)
+
+T = 1/Fs;            
+N = length(x);         
+t = (0:N-1)*T;      
+
+seq=0:N-1;  
+
+df=Fs/N; 
+f=(0:1:N/2)*df;  
+
+Y = fft(x); 
+P2 = abs(Y/N); 
+P1 = P2(1:N/2+1); 
+P1(2:end-1) = P1(2:end-1)*2;  
+
+[~, loca]=max(P1); 
+freq = f(loca);
+HR = roundn(freq*60,-2);
+
+end
+
